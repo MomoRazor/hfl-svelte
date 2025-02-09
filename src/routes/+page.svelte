66194bbc4ade072337fb5ea4e6d79ee40c045a1c
Maurovic Cachia <maurovic.cachia@gmail.com>
2025-02-09 11:54:15 +0100

    Initial Push of library

---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to the Human First Labs (HFL) Svelte Package</h1>
+<p>This package houses all the svelte code used in HFL work thus far, that is generic enough to be useful for different projects.</p>
