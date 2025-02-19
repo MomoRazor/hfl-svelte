289e7572504cd1a9d15a22dcfb9002095c9f06e3
Maurovic Cachia <maurovic.cachia@gmail.com>
2025-02-19 16:19:52 +0100

    Added content component

---
+++ src/lib/Content.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onMount, type Snippet } from 'svelte';
+	import { fade } from 'svelte/transition';
+
+	interface Props {
+		/** This is just the text */
+		children: Snippet;
+	}
+
+	const { children }: Props = $props();
+
+	let ready = $state(false);
+	onMount(() => (ready = true));
+</script>
+
+{#if ready}
+	<section
+		in:fade={{
+			delay: 0,
+			duration: 500
+		}}
+	>
+		{@render children()}
+	</section>
+{/if}
+
+<style>
+	section {
+		display: flex;
+		width: 100%;
+		flex-direction: column;
+		flex: 1;
+		box-sizing: border-box;
+	}
+</style>
